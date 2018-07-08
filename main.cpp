6fa80887278431b67772b32ac76d901564f55059
szczerbi <vszczerbinski@gmail.com>
2018-07-08 17:05:21 +0200

    Defined basic functions

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 
 #include <iostream>
+#include <vector>
+#include <map>
+#include <boost/optional.hpp>
+
+struct RGB
+{
+	unsigned r, g, b;
+};
+
+boost::optional<RGB> colorNameToRgb(std::string text)
+{
+	return boost::none;
+};
+
+std::map<RGB, unsigned> countColors(std::string path_to_file)
+{
+	return std::map<RGB, unsigned>();
+}
+
+void createPaletteImage(std::map<RGB, unsigned> palette);
 
 int main()
 {
